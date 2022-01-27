ec35be69aec46e751461cf38413331ae33bbbd35
Amit Kumar <46594792+ak672676@users.noreply.github.com>
2022-01-27 11:34:46 +0530

    HomePage added

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./sidebar.css";
-import { LineStyle, Timeline, TrendingUp,Person } from "@mui/icons-material";
+import { LineStyle, Timeline, TrendingUp,Person,Inventory,AttachMoney,Assessment } from "@mui/icons-material";
 
 export default function Sidebar() {
   return (
@@ -22,31 +22,19 @@ export default function Sidebar() {
         </div>
 
         <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Dashboard</h3>
-          <ul className="sidebarList">
-            <li className="sidebarListItem ">
-              <Person className="sidebarIcon" /> Home
-            </li>
-            <li className="sidebarListItem">
-              <Timeline className="sidebarIcon" /> Analytics
-            </li>
-            <li className="sidebarListItem">
-              <TrendingUp className="sidebarIcon" /> Sales
-            </li>
-          </ul>
-        </div>
-
-        <div className="sidebarMenu">
           <h3 className="sidebarTitle">Quick Menu</h3>
           <ul className="sidebarList">
             <li className="sidebarListItem ">
-              <LineStyle className="sidebarIcon" /> Home
+              <Person className="sidebarIcon" /> User
             </li>
             <li className="sidebarListItem">
-              <Timeline className="sidebarIcon" /> Analytics
+              <Inventory className="sidebarIcon" /> Products
             </li>
             <li className="sidebarListItem">
-              <TrendingUp className="sidebarIcon" /> Sales
+              <AttachMoney className="sidebarIcon" /> Transactions
+            </li>
+            <li className="sidebarListItem">
+              <Assessment className="sidebarIcon" /> Reports
             </li>
           </ul>
         </div>
